b1dc0a4480b3e000aabaa6c41f7803a8c24bae21
Waqar <waqar@My-Ubuntu.myguest.virtualbox.org>
2025-01-16 15:57:54 +0530

    token commit

--- main.tf
+++ main.tf
@@ -1,17 +1,10 @@
 provider "github" {
-  token = var.github_token  # This token will be passed from GitHub Actions as an environment variable
-  owner = "Waqar26-parkar"   # Replace with your GitHub username or organization
+  token = var.github_token
+  owner = "Waqar26-parkar"  # Use your personal account name here
 }
 
 resource "github_repository" "new_repo" {
   name        = "new-githubcicd-repository"
   description = "A new repository created by Terraform"
-  visibility  = "public"  # You can change this to "private" if you want
+  visibility  = "public"
 }
-
-resource "github_team" "new_team" {
-  name        = "dev-team"
-  description = "Development Team"
-}
-
-
